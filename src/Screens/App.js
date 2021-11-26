f0978a34b5e37be35b06ca87a9f4e00e903166d6
mamdouhramadan <mamdouhalamassi@gmail.com>
2021-11-26 16:43:24 +0400

    Init App , Prepare Files

---
+++ src/Screens/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="py-5">
+          <h3 className="text-center">Hello Github</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
